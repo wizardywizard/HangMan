824a094c9dc2f8a7cbb4eda921939022c83ac7e0
wizardywizard <littledavid175@gmail.com>
2022-06-18 16:00:09 -0700

    grabbed elemets from the DOM in the JS file

---
+++ app.js
@@ -0,0 +1,24 @@
+const letterContainer = document.getElementById("letter-container");
+const optionsContainer = document.getElementById("options-container");
+const userInputContainer = document.getElementById("user-input-container");
+const newGameContainer = document.getElementById("new-game-container");
+const newGameButton = document.getElementById("new-game-button");
+const canvas = document.getElementById("canvas");
+const resultText = document.getElementById("result-text");
+
+let options = {
+    friuts: [],
+    animals: [],
+    states: [],
+};
+
+
+
+
+
+
+
+
+
+
+
