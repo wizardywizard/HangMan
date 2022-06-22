5bb772e3ef9febb70b59d786f82637ed47fec76b
wizardywizard <littledavid175@gmail.com>
2022-06-21 19:33:35 -0700

    add chosen word ans styles to word

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
 const letterContainer = document.getElementById("letter-container");
 const optionsContainer = document.getElementById("options-container");
-const userInputContainer = document.getElementById("user-input-container");
+const userInputSection = document.getElementById("user-input-section");
 const newGameContainer = document.getElementById("new-game-container");
 const newGameButton = document.getElementById("new-game-button");
 const canvas = document.getElementById("canvas");
 const resultText = document.getElementById("result-text");
 
 let options = {
-    friuts: [
+    fruits: [
         "Apple",
         "Blueberry",
         "Pineapple",
@@ -45,23 +45,55 @@ const displayOptions = () => {
         buttonCon.innerHTML += `<button class="options" onclick="generateWord('${value}')">${value}</button>`;
     }
     optionsContainer.appendChild(buttonCon);
+};
+
+const blocker = () => {
+    let optionsButtons = document.querySelectorAll("options");
+    let letterButtons = document.querySelectorAll("letters");
+    optionsButtons.forEach((button) =>{
+        button.disabled = true;
+    });
+
+    letterButtons.forEach((button) => {
+        button.disabled.true;;
+    });
+    newGameContainer.classList.remove("hide");
 }
 
 const generateWord = (optionValue) => {
     let optionsButtons = document.querySelectorAll(".options");
     optionsButtons.forEach((button) => {
-        if (button,innerText.toLowerCase() === optionValue){
-            button.classList.add("active";)
+        if (button.innerText.toLowerCase() === optionValue) {
+            button.classList.add("active");
         }
         button.disabled = true;
-    })
-}
+    });
+
+    letterContainer.classList.remove("hide");
+    userInputSection.innerText = "";
+
+    let optionArray = options[optionValue];
+    chosenWord = optionArray[Math.floor(Math.random() * optionArray.length)];
+    chosenWord = chosenWord.toUpperCase();
+    console.log(chosenWord);
+
+    let displayItem = chosenWord.replace(/./g, '<span class="dashes">_</span>');
+    userInputSection.innerHTML = displayItem;
+};
 
 const initializer = () => {
     winCount = 0;
     count = 0;
+
+    for (let i = 65; i < 91; i++) {
+        let button = document.createElement("button");
+        button.classList.add("letters");
+        button.innerText = String.fromCharCode(i);
+        letterContainer.append(button);
+    };
+
     displayOptions();
-}
+};
 
 newGameButton.addEventListener("click", initializer);
 window.onload = initializer;
