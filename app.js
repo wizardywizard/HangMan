88e500b2b62ff007bfc85a2773f373c38a84a009
wizardywizard <littledavid175@gmail.com>
2022-06-22 19:58:19 -0700

    added drawing  function and can win or lose

--- app.js
+++ app.js
@@ -85,15 +85,125 @@ const initializer = () => {
     winCount = 0;
     count = 0;
 
+    userInputSection.innerHTML = "";
+    optionsContainer.innerHTML = "";
+    letterContainer.classList.add("hide");
+    newGameContainer.classList.add("hide");
+    letterContainer.innerHTML = "";
+
     for (let i = 65; i < 91; i++) {
         let button = document.createElement("button");
         button.classList.add("letters");
         button.innerText = String.fromCharCode(i);
+        button.addEventListener("click", () => {
+          let charArray = chosenWord.split("");
+          let dashes = document.getElementsByClassName("dashes");
+          if (charArray.includes(button.innerText)) {
+            charArray.forEach((char, index) => {
+              if (char === button.innerText) {
+                dashes[index].innerText = char;
+                winCount += 1;
+                if (winCount == charArray.length) {
+                  resultText.innerHTML = `<h2
+                   class='win-msg'>You Win!!</h2>
+                   <p>The word was <span>${chosenWord}</span></p>`;
+                  blocker();
+                }
+              }
+            });
+          } else {
+            count += 1;
+            drawMan(count);
+            if (count == 6) {
+              resultText.innerHTML = `<h2 
+              class='lose-msg'>You Lose!!</h2>
+              <p>The word was <span>${chosenWord}</span></p>`;
+              blocker();
+            }
+          }
+          button.disabled = true;
+        });
         letterContainer.append(button);
+      }
+      displayOptions();
+      let { initialDrawing } = canvasCreator();
+      initialDrawing();
+};
+
+const canvasCreator = () => {
+    let context = canvas.getContext("2d");
+    context.beginPath();
+    context.strokeStyle = "#000";
+    context.lineWidth = 2;
+
+    const drawLine = (fromX, fromY, toX, toY) => {
+        context.moveTo(fromX, fromY);
+        context.lineTo(toX, toY);
+        context.stroke();
     };
 
-    displayOptions();
+    const head = () => {
+        context.beginPath();
+        context.arc(70, 30, 10, 0, Math.PI * 2, true);
+        context.stroke();
+    };
+
+    const body = () => {
+        drawLine(70, 40, 70, 80);
+    };
+
+    const leftArm = () => {
+        drawLine(70, 40, 70, 80);
+    };
+
+    const rightArm = () => {
+        drawLine(70, 40, 70, 80);
+    };
+
+    const leftLeg = () => {
+        drawLine(70, 40, 70, 80);
+    };
+
+    const rightLeg = () => {
+        drawLine(70, 40, 70, 80);
+    };
+
+    const initialDrawing = () => {
+        context.clearRect(0, 0, context.canvas.width, context.canvas.height);
+        drawLine(10, 130, 130, 130);
+        drawLine(10, 10, 10, 131);
+        drawLine(10, 10, 70, 10);
+        drawLine(70, 10, 70, 20);
+    };
+
+    return { initialDrawing, head, body, leftArm, rightArm, leftLeg, rightLeg };
 };
+
+const drawMan = (count) => {
+    let { head, body, leftArm, rightArm, leftLeg, rightLeg } = canvasCreator();
+    switch (count) {
+      case 1:
+        head();
+        break;
+      case 2:
+        body();
+        break;
+      case 3:
+        leftArm();
+        break;
+      case 4:
+        rightArm();
+        break;
+      case 5:
+        leftLeg();
+        break;
+      case 6:
+        rightLeg();
+        break;
+      default:
+        break;
+    }
+  };
 
 newGameButton.addEventListener("click", initializer);
 window.onload = initializer;
